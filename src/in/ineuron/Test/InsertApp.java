a13d72f5645befe4da0cc8c937825caa44147e40
Chandrakala <chandrakala.sk93@gmail.com>
2023-10-04 17:16:44 +0530

    Hibernate connection pool

---
+++ src/in/ineuron/Test/InsertApp.java
@@ -0,0 +1,63 @@
+package in.ineuron.Test;
+
+import java.io.IOException;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import in.ineuron.Model.Student;
+import in.ineuron.Util.HibernateUtil;
+
+public class InsertApp {
+
+	public static void main(String[] args) throws IOException {
+		
+		Session session=null;
+		Transaction transaction=null;
+		boolean flag = false;
+		
+		try {
+		session=HibernateUtil.geSession();
+		
+		if(session !=null)
+		{
+			transaction = session.beginTransaction();
+			if(transaction != null)
+			{
+				Student student = new Student();
+				student.setSname("hyder");
+				student.setSaddress("RCB");
+				student.setSage(28);
+				session.save(student);
+				flag=true;
+				
+			}
+			
+		}
+		}catch (HibernateException e) {
+			e.printStackTrace();
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+			
+		}finally {
+			if(flag) {
+				transaction.commit();
+			System.out.println("Object saved to database");
+			
+			
+			}
+			else {
+				transaction.rollback();
+				System.out.println("Object not saved to database");
+			
+			}
+			
+			HibernateUtil.closeSession(session);
+			HibernateUtil.closeSessionFactory();
+		}
+
+	}
+
+}
